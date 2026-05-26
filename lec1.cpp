c2dddf3564800d2386bb51156a4dfac77e1899fa
siddharth shekhawat <siddharth.23bcon1745@jecrcu.edu.in>
2026-05-26 11:55:33 +0530

    Update lec1.cpp

--- lec1.cpp
+++ lec1.cpp
@@ -1,17 +1,17 @@
 //finding the the upper case and lower case
-// #include<iostream>
-// using namespace std;
-// int main(){
-//   char ch;
-//   if(ch>='a' && ch<='z'){
-//     cout<<"lowecase";
+#include<iostream>
+using namespace std;
+int main(){
+  char ch;
+  if(ch>='a' && ch<='z'){
+    cout<<"lowecase";
 
-//   }
-//   else{
-//     cout<<"upper case";
-//   }
-//   return 0;
-// }
+  }
+  else{
+    cout<<"upper case";
+  }
+  return 0;
+}
 the uses of do while loop for the printing of value count 
 #include<iostream>
 using namespace std;
@@ -29,3 +29,41 @@ int main(){
   cout<<endl;
   return 0;
 }
+//patten of square
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<n;
+  cin>>n;
+  for(int i =1;i<=n;i++){
+    for(int j =1;j<=n;j++){
+      cout<<j;
+
+    }
+    cout<<endl;
+  }
+  return 0;
+}
+//printing the character
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<n;
+  cin>>n;
+  for(int i =1;i<=n;i++){
+    char ch = 'a';
+    for(int j =1;j<=n;j++){
+      cout<<ch;
+      ch=ch+1;
+
+    }
+    cout<<endl;
+  }
+  return 0;
+}
+
+  
+  
+ 
