f1e706ac4ce4f9aacb9e754c1d564b800621f978
siddharth shekhawat <siddharth.23bcon1745@jecrcu.edu.in>
2026-05-26 10:58:53 +0530

    Refactor to use while loop for counting
    Replaced character case checking with a do-while loop to print numbers from 1 to 20.

--- lec1.cpp
+++ lec1.cpp
@@ -1,14 +1,31 @@
 //finding the the upper case and lower case
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   char ch;
+//   if(ch>='a' && ch<='z'){
+//     cout<<"lowecase";
+
+//   }
+//   else{
+//     cout<<"upper case";
+//   }
+//   return 0;
+// }
+the uses of do while loop for the printing of value count 
 #include<iostream>
 using namespace std;
 int main(){
-  char ch;
-  if(ch>='a' && ch<='z'){
-    cout<<"lowecase";
-
+  int n = 20;
+  int count = 1;
+ 
+  while (count<=n)
+  {
+    cout<<count<<" ";
+    count++;
   }
-  else{
-    cout<<"upper case";
-  }
+  
+  
+  cout<<endl;
   return 0;
 }
